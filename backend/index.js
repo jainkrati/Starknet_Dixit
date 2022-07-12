eb34548bb7450be8421ed870f87d2b0437ef12cb
Shivam Nagar <shivam.nagar@outlook.com>
2022-07-12 11:36:41 +0530

    API fixes and added postman collection

--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,52 @@
 const Express = require('express');
 const { v4: uuidv4 } = require('uuid');
+var bodyParser = require('body-parser'); 
 
 let app = Express();
 let port = 3001; 
 
 let lobbies = {};
 
-app.use(Express.bodyParser());
+app.use(bodyParser.json());
 
 app.post('/lobby', (req, res) => {
-  let user_id=req.body('user_id', null);
-  let lobby_id = req.body('lobby_id',uuidv4());
-  let lobby_obj = lobbies[lobby];
-  if(!lobby_obj){
-    lobby_obj = {
+  let user_id=req.body.user_id;
+  let lobby_id = req.body.lobby_id;
+  if(!lobbies[lobby_id]){
+    lobbies[lobby_id] = {
       users: [user_id]
     }
   } else {
-    lobby_obj.users.push(user_id);
+    lobbies[lobby_id].users.push(user_id);
   }
   res.send({
     lobby_id
   });
 });
 
+app.get('/lobby', (req, res) => {
+  let lobby_id = req.query.lobby_id;
+  let lobby_obj = lobbies[lobby_id];
+  res.send({
+    lobby_obj
+  });
+});
+
 app.post('/start', (req, res) => {
-  let user_id = req.body('user_id', null);
-  let lobby_id = req.body('lobby_id', null);
+  let user_id = req.body.user_id;
+  let lobby_id = req.body.lobby_id;
   let lobby_obj = lobbies[lobby_id];
   if(!lobby_obj || lobby_obj.users[0]!=user_id) {
     res.send({
       "error": true,
       "message": "Invalid lobby"
     });
+    return;
   }
-  lobby_obj.status = "start";
-  lobby_obj.nominated_user_id = lobby_obj.users[0];
+  lobbies[lobby_id].status = "start";
+  lobbies[lobby_id].nominated_user_id = lobbies[lobby_id].users[0];
   res.send({
-    nominated_user_id: lobby_obj.nominated_user_id
+    nominated_user_id: lobbies[lobby_id].nominated_user_id
   })
 })
 
@@ -46,36 +55,39 @@ app.get('/cards', (req, res) => {
 })
 
 app.post('/round/start', (req, res) => {
-  let user_id = req.body('user_id', null);
-  let lobby_id = req.body('lobby_id', null);
-  let card_id = req.body('card_id', null);
-  let round_description = req.body('round_description', null);
+  let user_id = req.body.user_id;
+  let lobby_id = req.body.lobby_id;
+  let card_id = req.body.card_id;
+  let round_description = req.body.round_description;
   let lobby_obj = lobbies[lobby_id];
-  if(lobby_obj.status !== "start") {
+  if(!lobby_obj || lobby_obj.status !== "start") {
     res.send({
       "error": true,
       "message": "Invalid game state"
     });
+    return;
   }
   if(user_id !== lobby_obj.nominated_user_id) {
     res.send({
       "error": true,
       "message": "Invalid nominated player"
     });
+    return;
   }
-  lobby_obj.status = "nominated";
-  lobby_obj.current_round.player_card_mapping[user_id] = card_id;
-  lobby_obj.current_round.round_description = round_description;
+  lobbies[lobby_id].status = "nominated";
+  lobbies[lobby_id].current_round.player_card_mapping[user_id] = card_id;
+  lobbies[lobby_id].current_round.round_description = round_description;
 })
 
 app.get('/round/status', (req, res) => {
-  let lobby_id = req.query('lobby_id', null);
+  let lobby_id = req.query;
   let lobby_obj = lobbies[lobby_id];
   if(!lobby_obj || lobby_obj.status !== "nominated") {
     res.send({
       "error": true,
       "message": "Invalid game state or no description added"
     });
+    return;
   }
   let round_description = lobby_obj.current_round.round_description;
   res.send({
@@ -85,10 +97,17 @@ app.get('/round/status', (req, res) => {
 });
 
 app.post('/round/guess', (req, res) => {
-  let user_id = req.body('user_id', null);
-  let lobby_id = req.body('lobby_id', null);
-  let card_id = req.body('card_id', null);
+  let user_id = req.body.user_id;
+  let lobby_id = req.body.lobby_id;
+  let card_id = req.body.card_id;
   let lobby_obj = lobbies[lobby_id];
+  if(!lobby_obj) {
+    res.send({
+      "error": true,
+      "message": "Invalid lobby"
+    });
+    return;
+  }
   if(lobby_obj.status !== "nominated") {
     res.send({
       "error": true,
@@ -101,7 +120,10 @@ app.post('/round/guess', (req, res) => {
       "message": "player has already sumitted a card"
     });
   }
-  lobby_obj.current_round.player_card_mapping[user_id] = card_id;
+  lobbies[lobby_id].current_round.player_card_mapping[user_id] = card_id;
+  res.send({
+    "status": "success"
+  })
 })
 
 // Voting based on card ID and scoring based on user_id.
